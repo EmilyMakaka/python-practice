c81fe9b58ebfeda1c6583aa4b180c6d12df70a80
EmilyMakaka <emily.makaka@strathmore.edu>
2025-04-25 10:20:30 +0300

    Create even_or_odd_number.py
    a function to check if a number is even or odd using the modulo operator

---
+++ even_or_odd_number.py
@@ -0,0 +1,8 @@
+# A function that checks if a number is odd or even
+def even_or_odd(number):
+# if...else statment to determine if it is odd or even
+#if it is divisible by 2 with no reminder it is even
+if number % 2 == 0:
+   return "Even"
+else: # oterwise it is odd
+  return "Odd"
