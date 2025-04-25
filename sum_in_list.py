c0e05d9ec6f7445eab9ef77c083edd554eb3707f
EmilyMakaka <emily.makaka@strathmore.edu>
2025-04-25 09:56:20 +0300

    Create sum_in_list.py
    simple function using a for-loop with inline comments

---
+++ sum_in_list.py
@@ -0,0 +1,7 @@
+#A function that sums all elements in a list
+def sum_in_list(elements):
+ total=0
+	# for loop that iterates each element in the list
+for element in elements:
+	total = total + element
+	return total #returns the final sum
